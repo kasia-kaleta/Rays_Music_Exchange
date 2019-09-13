11da31b5e16c794ee7a9cd68fde255eeff008d6f
kat3005 <kasia.alicja@gmail.com>
2019-09-13 12:53:42 +0100

    Adds drums class

---
+++ src/main/java/instruments/Drums.java
@@ -0,0 +1,19 @@
+package instruments;
+
+public class Drums extends MusicInstrument {
+
+
+    private int noOfCymbals;
+
+
+    public Drums(String name, String material, double buyingPrice, double sellingPrice, int noOfCymbals){
+        super(name, material, buyingPrice, sellingPrice);
+        this.noOfCymbals = noOfCymbals;
+    }
+
+    public int getNoOfCymbals() {
+        return noOfCymbals;
+    }
+
+
+}
---
+++ src/test/java/DrumsTest.java
@@ -0,0 +1,40 @@
+import instruments.Drums;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DrumsTest {
+
+    Drums drums;
+
+    @Before
+    public void before(){
+        drums = new Drums("Yamaha Rydeen", "Metal & Poplar", 295.00, 356.00, 2);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Yamaha Rydeen", drums.getName());
+    }
+
+    @Test
+    public void canGetMaterial(){
+        assertEquals("Metal & Poplar", drums.getMaterial());
+    }
+
+    @Test
+    public void canGetBuyingPrice(){
+        assertEquals(295.00, drums.getBuyingPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetSellingPrice(){
+        assertEquals(356.00, drums.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetNoOfCymbals(){
+        assertEquals(2, drums.getNoOfCymbals());
+    }
+}
